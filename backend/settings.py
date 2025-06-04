bbaa4151749c421bff50ed3252fb9fff05697467
Bohdan337 <bogdansem37@gmail.com>
2025-06-04 18:07:08 +0300

    fix origins

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "*"), "localhost", "127.0.0.1"]
 
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
