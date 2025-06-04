ead29e5752ee72f42b6ec1a09839a08dba8a4a66
Bohdan337 <bogdansem37@gmail.com>
2025-06-04 18:16:42 +0300

    remove CORS_ALLOW

--- backend/settings.py
+++ backend/settings.py
@@ -39,12 +39,12 @@ FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000",
-    "http://localhost:3000",
-    'https://samtalk-frontend.onrender.com',
-    FRONTEND_URL,
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://127.0.0.1:3000",
+#     "http://localhost:3000",
+#     'https://samtalk-frontend.onrender.com',
+#     FRONTEND_URL,
+# ]
 
 # Application definition
 
