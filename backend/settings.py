dbf28b31e05fb7471648e4ba5ada13cdf4088723
Bohdan337 <bogdansem37@gmail.com>
2025-06-04 18:11:25 +0300

    add url CORS_ALLOWED

--- backend/settings.py
+++ backend/settings.py
@@ -42,6 +42,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    'https://samtalk-frontend.onrender.com',
     FRONTEND_URL,
 ]
 
